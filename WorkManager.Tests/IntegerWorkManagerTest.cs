2f46d25a3cdec1c740defc7af5073899c64cc239
Clark Stuth <clarkstuth@gmail.com>
2014-10-11 18:29:43 -0500

    Added new tests

---
+++ WorkManager.Tests/IntegerWorkManagerTest.cs
@@ -0,0 +1,55 @@
+﻿using System.ServiceModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Telerik.JustMock;
+using WorkManager.DataContracts;
+
+namespace WorkManager.Tests
+{
+    [TestClass]
+    public class IntegerWorkManagerTest
+    {
+        IntegerWorkManager Manager { get; set; }
+        OperationContext Context { get; set; }
+        ICommunicationObject CommunicationObject { get; set; }
+        IWorker WorkerCallback { get; set; }
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            Context = Mock.Create<OperationContext>();
+            CommunicationObject = Mock.Create<ICommunicationObject>();
+            WorkerCallback = Mock.Create<IWorker>();
+
+            Mock.Arrange(() => Context.GetCallbackChannel<ICommunicationObject>()).Returns(CommunicationObject);
+            Mock.Arrange(() => Context.GetCallbackChannel<IWorker>()).Returns(WorkerCallback);
+
+            Manager = new IntegerWorkManager();
+            Manager.SetOperationContext(Context);
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            Manager = null;
+            WorkerCallback = null;
+            CommunicationObject = null;
+            Context = null;
+
+            Mock.Reset();
+        }
+
+        [TestMethod]
+        public void StartWorkingShouldAddConnectedClientToPoolOfAvailableClients()
+        {
+            var expectedCount = 1;
+
+            Manager.StartWorking();
+
+            var workerCount = Manager.GetWorkers().Count;
+            Assert.AreEqual(expectedCount, workerCount);
+        }
+
+
+
+    }
+}
