66dafd81c8d204ecbbce1cf266f82170650d535f
Clark Stuth <clarkstuth@gmail.com>
2014-10-11 20:10:28 -0500

    More new items

---
+++ WorkManagerApplication.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WorkManagerApplication.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Telerik AD")]
+[assembly: AssemblyProduct("WorkManagerApplication.Tests")]
+[assembly: AssemblyCopyright("Copyright @ Telerik 2002-2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6a6c7ef0-caa0-44d9-a6c5-2b3b6a6f076b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("123.4.5.0")]
+[assembly: AssemblyFileVersion("123.4.5.0")]
---
+++ WorkManagerApplication/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.ServiceModel;
+using WorkManager;
+
+namespace WorkManagerApplication
+{
+    public class Program
+    {
+        public static ServiceHost IntegerWebServiceHost { get; set; }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                CreateWebServiceHost();
+                StartWebServiceHost();
+
+                DisplayStartupMessage();
+                PassUserInputToWorkManager();
+            }
+            finally
+            {
+                CloseWebServiceHost();
+            }
+        }
+
+        static void CreateWebServiceHost()
+        {
+            IntegerWebServiceHost = new ServiceHost(typeof(IntegerWorkManager));
+        }
+
+        static void StartWebServiceHost()
+        {
+            Console.WriteLine("Starting web service host...");
+            IntegerWebServiceHost.Open();
+        }
+
+        static void CloseWebServiceHost()
+        {
+            if (IntegerWebServiceHost.State == CommunicationState.Opened ||
+                IntegerWebServiceHost.State == CommunicationState.Opening)
+            {
+                Console.WriteLine("Closing web service host...");
+                IntegerWebServiceHost.Close();
+            }
+        }
+
+        static void DisplayStartupMessage()
+        {
+            Console.WriteLine("---------");
+            Console.WriteLine("Integer Work Manager web service has started.");
+            Console.WriteLine("To submit work to the service it, type a comma delimited line of integers.");
+            Console.WriteLine("Work will be distributed from highest integer to lowest integer");
+            Console.WriteLine("Type 'quit' to exit.");
+            Console.WriteLine("---------");
+        }
+
+        static void PassUserInputToWorkManager()
+        {
+            while (true)
+            {
+                var input = Console.ReadLine();
+                if (input == "quit")
+                {
+                    return;
+                }
+                else
+                {
+                    
+                }
+            }
+        }
+
+    }
+}
