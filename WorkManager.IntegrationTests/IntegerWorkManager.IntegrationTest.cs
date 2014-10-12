1362caddf72a9cac668d263b95b5af0e137efc5d
Clark Stuth <clarkstuth@gmail.com>
2014-10-11 19:22:21 -0500

    Started implementing an integration test, will have to revisit.

--- WorkManager.IntegrationTests/IntegerWorkManager.IntegrationTest.cs
+++ WorkManager.IntegrationTests/IntegerWorkManager.IntegrationTest.cs
@@ -1,10 +1,26 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.ServiceModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WorkManager.IntegrationTests.IntegerWorkManager;
 
 namespace WorkManager.IntegrationTests
 {
     [TestClass]
-    public class IntegerWorkManager
+    public class IntegerWorkManagerTest
     {
-        
+        [TestMethod]
+        public void IntegerWorkManagerShouldRemoveWorkerOnSuddenDisconnect()
+        {
+            var worker = new WorkerCallback();
+            var instanceContext = new InstanceContext(worker);
+            var client = new WorkManagerClient(instanceContext);
+
+            //connect client, ensure this was recorded.
+            client.StartWorking();
+            Assert.AreEqual(1, WorkManager.IntegerWorkManager.Workers.Count);
+            
+            //disconnect client without stopping working, ensure this was also recorded.
+            client.Close();
+            Assert.AreEqual(0, WorkManager.IntegerWorkManager.Workers.Count);
+        }
     }
 }
---
+++ WorkManager.IntegrationTests/WorkerCallback.cs
@@ -0,0 +1,12 @@
+﻿using WorkManager.IntegrationTests.IntegerWorkManager;
+
+namespace WorkManager.IntegrationTests
+{
+    public class WorkerCallback : IWorkManagerCallback
+    {
+        public void DoWork(int number)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
